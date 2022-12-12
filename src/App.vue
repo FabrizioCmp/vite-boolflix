26f767887d05749c50bbda99c6c7c1c2361c0bad
FabrizioCmp <fabriziocampari01@gmail.com>
2022-12-12 15:03:50 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue"
+
+export default{
+    components: {AppHeader, AppMain},
+    data(){
+        return{
+            
+        }
+    }
+}
+
+</script>
+
+
+<style lang="scss">
+@use "./style/general.scss";
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,21 @@
+<template>
+
+</template>
+
+<script>
+import AppCard from "./AppCard.vue";
+
+export default{
+    components:{AppCard},
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
